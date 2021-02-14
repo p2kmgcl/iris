1bffa9f43eff70680cd6a0566795e6b08f05738d
Pablo Molina <contact@pablomolina.me>
2021-02-14 18:17:37 +0100

    build: Write commitlint config in JSON

--- commitlint.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-env node */
-
-module.exports = {
-  extends: ['@commitlint/config-angular'],
-  rules: {
-    'subject-case': [2, 'always', 'sentence-case'],
-  },
-};
