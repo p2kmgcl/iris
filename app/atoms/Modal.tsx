07b80fd4f5a94ad0e9e192ac09d696ba2d4aba45
Pablo Molina <contact@pablomolina.me>
2021-02-17 18:51:21 +0100

    fix: Export ReactNode in Modal

--- app/atoms/Modal.tsx
+++ app/atoms/Modal.tsx
@@ -2,12 +2,15 @@ import React, { FC } from 'react';
 import { createPortal } from 'react-dom';
 import styles from './Modal.css';
 
-const Modal: FC = ({ children }) =>
-  createPortal(
-    <div className={styles.modal}>
-      <div>{children}</div>
-    </div>,
-    document.body,
-  );
+const Modal: FC = ({ children }) => (
+  <>
+    {createPortal(
+      <div className={styles.modal}>
+        <div>{children}</div>
+      </div>,
+      document.body,
+    )}
+  </>
+);
 
 export default Modal;
