f0dc58482f8555e346ca7d11db997cfc73b34212
Pablo Molina <contact@pablomolina.me>
2021-02-19 16:08:28 +0100

    feat: Add video icon

--- app/atoms/PhotoThumbnail.tsx
+++ app/atoms/PhotoThumbnail.tsx
@@ -1,17 +1,24 @@
 import React, { FC } from 'react';
 import { LoadedPhoto } from '../utils/PhotoLoader';
+import styles from './PhotoThumbnail.css';
+import { MdLocalMovies } from 'react-icons/all';
 
-const PhotoThumbnail: FC<{ photo: LoadedPhoto }> = ({ photo }) => (
-  <div
-    role="img"
-    style={{
-      backgroundImage: `url(${photo.thumbnailURL})`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center center',
-      width: '100%',
-      height: '100%',
-    }}
-  />
-);
+const PhotoThumbnail: FC<{ photo: LoadedPhoto }> = ({ photo }) => {
+  return (
+    <div className={styles.wrapper}>
+      <div
+        role="img"
+        className={styles.thumbnail}
+        style={{ backgroundImage: `url(${photo.thumbnailURL})` }}
+      />
+
+      {photo.isVideo ? (
+        <div className={styles.icon}>
+          <MdLocalMovies />
+        </div>
+      ) : null}
+    </div>
+  );
+};
 
 export default PhotoThumbnail;
