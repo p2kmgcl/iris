92015e1a0a23d6b531fcb45eac17821b46941d73
Pablo Molina <pablo.molina@liferay.com>
2021-02-23 18:50:46 +0100

    refactor: Update horizontal list stories

--- app/atoms/HorizontalList.stories.tsx
+++ app/atoms/HorizontalList.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HorizontalList, { ItemProps } from './HorizontalList';
 
 export default {
@@ -30,19 +30,69 @@ function Item(props: ItemProps) {
   );
 }
 
-export const Default = () => <HorizontalList itemCount={10} Item={Item} />;
-export const OneHundred = () => <HorizontalList itemCount={100} Item={Item} />;
-export const TenHundred = () => <HorizontalList itemCount={1000} Item={Item} />;
+export const Default = () => (
+  <HorizontalList
+    index={0}
+    onIndexChange={() => {}}
+    itemCount={10}
+    Item={Item}
+  />
+);
+export const OneHundred = () => (
+  <HorizontalList
+    index={0}
+    onIndexChange={() => {}}
+    itemCount={100}
+    Item={Item}
+  />
+);
+export const TenHundred = () => (
+  <HorizontalList
+    index={0}
+    onIndexChange={() => {}}
+    itemCount={1000}
+    Item={Item}
+  />
+);
 export const TenThousand = () => (
-  <HorizontalList itemCount={10000} Item={Item} />
+  <HorizontalList
+    index={0}
+    onIndexChange={() => {}}
+    itemCount={10000}
+    Item={Item}
+  />
 );
 export const OneMillion = () => (
-  <HorizontalList itemCount={1000000} Item={Item} />
+  <HorizontalList
+    index={0}
+    onIndexChange={() => {}}
+    itemCount={1000000}
+    Item={Item}
+  />
 );
 
-export const WithInitialScrollIndex = () => (
-  <HorizontalList initialIndex={500} itemCount={1000} Item={Item} />
-);
+export const WithControlledScrollIndex = () => {
+  const [index, setIndex] = useState(500);
+
+  return (
+    <div style={{ width: '100%', height: '100%' }}>
+      <input
+        type="number"
+        min={0}
+        max={999}
+        value={index}
+        onChange={(event) => setIndex(Number(event.target.value))}
+      />
+
+      <HorizontalList
+        index={index}
+        onIndexChange={setIndex}
+        itemCount={1000}
+        Item={Item}
+      />
+    </div>
+  );
+};
 
 export const WithHeader = () => (
   <div
@@ -57,7 +107,12 @@ export const WithHeader = () => (
   >
     <h1>Nice grid</h1>
     <div style={{ flexGrow: 1 }}>
-      <HorizontalList itemCount={100} Item={Item} />
+      <HorizontalList
+        index={0}
+        onIndexChange={() => {}}
+        itemCount={100}
+        Item={Item}
+      />
     </div>
   </div>
 );
