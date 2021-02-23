70a6dda642b6b0768cc86ba358e299678ad2096a
Pablo Molina <pablo.molina@liferay.com>
2021-02-23 19:56:26 +0100

    refactor: Extract SetupStep

--- app/atoms/SetupStep.tsx
+++ app/atoms/SetupStep.tsx
@@ -1,10 +1,13 @@
 import React, { FC } from 'react';
 import styles from './SetupStep.css';
 
-const SetupStep: FC = ({ children }) => {
+const SetupStep: FC<{ title: string }> = ({ title, children }) => {
   return (
     <article className={styles.wrapper}>
-      <div className={styles.content}>{children}</div>
+      <div className={styles.content}>
+        <h1 className={styles.title}>{title}</h1>
+        {children}
+      </div>
     </article>
   );
 };
--- app/atoms/SetupStepTitle.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from 'react';
-import styles from './SetupStepTitle.css';
-
-const SetupStepTitle: FC = ({ children }) => {
-  return <h1 className={styles.title}>{children}</h1>;
-};
-
-export default SetupStepTitle;
