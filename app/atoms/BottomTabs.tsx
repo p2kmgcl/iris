fbde5a274baa46c4bbcd418478485db3e8c2f107
Pablo Molina <contact@pablomolina.me>
2021-02-27 11:52:38 +0100

    feat: Accessible BottomTabs

--- app/atoms/BottomTabs.tsx
+++ app/atoms/BottomTabs.tsx
@@ -1,4 +1,4 @@
-import React, { FC, Suspense } from 'react';
+import React, { FC, Suspense, useLayoutEffect, useRef } from 'react';
 import styles from './BottomTabs.css';
 import Spinner from './Spinner';
 
@@ -14,57 +14,66 @@ const BottomTabs: FC<{
     Icon: FC;
     Content: FC;
   }>;
-}> = ({ selectedTabId, onTabClick, tabs }) => (
-  <div className={styles.wrapper}>
-    <div className={styles.tabs} role="tablist">
-      {tabs.map((tab) => (
-        <button
-          className={styles.tab}
-          key={tab.tabId}
-          type="button"
-          role="tab"
-          aria-selected={tab.tabId === selectedTabId}
-          aria-controls={`${TAB_ID_PREFIX}${tab.tabId}`}
-          onClick={() => onTabClick(tab.tabId)}
-        >
-          <span className={styles.tabIcon}>
-            <tab.Icon />
-          </span>
-          <span
-            className={styles.tabLabel}
-            id={`${LABEL_ID_PREFIX}${tab.tabId}`}
-          >
-            {tab.label}
-          </span>
-        </button>
-      ))}
-    </div>
+}> = ({ selectedTabId, onTabClick, tabs }) => {
+  const tabRef = useRef<HTMLDivElement>(null);
 
-    <div className={styles.content}>
-      {tabs.map((tab) =>
-        tab.tabId === selectedTabId ? (
-          <div
-            key={tab.tabId}
-            className={styles.tabContent}
-            tabIndex={0}
-            role="tabpanel"
-            id={`${TAB_ID_PREFIX}${tab.tabId}`}
-            aria-labelledby={`${LABEL_ID_PREFIX}${tab.tabId}`}
-          >
-            <Suspense
-              fallback={
-                <div className={styles.tabSpinner}>
-                  <Spinner size="large" />
-                </div>
-              }
+  useLayoutEffect(() => {
+    tabRef.current?.focus();
+  }, [selectedTabId]);
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.content}>
+        {tabs.map((tab) =>
+          tab.tabId === selectedTabId ? (
+            <div
+              key={tab.tabId}
+              className={styles.tabContent}
+              tabIndex={0}
+              role="tabpanel"
+              id={`${TAB_ID_PREFIX}${tab.tabId}`}
+              aria-labelledby={`${LABEL_ID_PREFIX}${tab.tabId}`}
+              ref={tabRef}
             >
-              <tab.Content />
-            </Suspense>
-          </div>
-        ) : null,
-      )}
+              <Suspense
+                fallback={
+                  <div className={styles.tabSpinner}>
+                    <Spinner size="large" />
+                  </div>
+                }
+              >
+                <tab.Content />
+              </Suspense>
+            </div>
+          ) : null,
+        )}
+      </div>
+
+      <div className={styles.tabs} role="tablist">
+        {tabs.map((tab) => (
+          <button
+            className={styles.tab}
+            key={tab.tabId}
+            type="button"
+            role="tab"
+            aria-selected={tab.tabId === selectedTabId}
+            aria-controls={`${TAB_ID_PREFIX}${tab.tabId}`}
+            onClick={() => onTabClick(tab.tabId)}
+          >
+            <span className={styles.tabIcon}>
+              <tab.Icon />
+            </span>
+            <span
+              className={styles.tabLabel}
+              id={`${LABEL_ID_PREFIX}${tab.tabId}`}
+            >
+              {tab.label}
+            </span>
+          </button>
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default BottomTabs;
