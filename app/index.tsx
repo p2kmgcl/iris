af606d64405882eb0e916c8e3bcbb5ef8fca2480
Pablo Molina <contact@pablomolina.me>
2021-02-28 18:42:36 +0100

    feat: Split Setup components

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,15 @@
 import './index.css';
+import React, { Suspense } from 'react';
 import { render } from 'react-dom';
 import Database from './utils/Database';
 import ScanContextProvider from './contexts/ScanContext';
 import IconStyleContextProvider from './contexts/IconStyleContext';
 import Authentication from './utils/Authentication';
 import App from './organisms/App';
-import Setup from './organisms/Setup';
 
 const appElement = document.getElementById('app') as HTMLDivElement;
+const LazyApp = React.lazy(() => Promise.resolve({ default: App }));
+const LazySetup = React.lazy(() => import('./organisms/Setup'));
 
 async function registerServiceWorker() {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
@@ -31,12 +33,14 @@ async function registerServiceWorker() {
         (value) => !!value,
       );
 
-    const MainComponent = isSetupReady ? App : Setup;
+    const MainComponent = isSetupReady ? LazyApp : LazySetup;
 
     render(
       <IconStyleContextProvider>
         <ScanContextProvider>
-          <MainComponent />
+          <Suspense fallback={<></>}>
+            <MainComponent />
+          </Suspense>
         </ScanContextProvider>
       </IconStyleContextProvider>,
       appElement,
